feeab31cd68a19a2aa7902bb9f7baa56f808006e
Chris533 <ganchengs@gmail.com>
2020-09-30 12:23:50 +0800

    Update components/PluginTable (use handlePluginPath)

--- components/PluginTable.tsx
+++ components/PluginTable.tsx
@@ -3,14 +3,14 @@ import dynamic from "next/dynamic"
 import {
   PluginTableProps,
   TableSkeleton,
-  handleSpecialPlugin
+  handlePluginPath
 } from "@bunred/bunadmin"
 
 function PluginTable({ team, group, name, hideLoading }: PluginTableProps) {
-  const pluginPath = handleSpecialPlugin({ team, group, name })
+  const pluginPath = handlePluginPath({ team, group, name })
 
   const Plugin = dynamic({
-    loader: () => import(`../plugins/${pluginPath}`),
+    loader: () => import(`../plugins/dynamic/${pluginPath}`),
     loading: () =>
       hideLoading ? null : <TableSkeleton title={`${name} loading...`} />
   })
